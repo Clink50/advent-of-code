74efd7c7bcd7593f451a8bc63ec4c32775ede222
Travis Clinkscales <clink73@gmail.com>
2021-12-10 16:49:10 -0600

    05 - Part 1 complete, takes about 30 seconds to get the answer

---
+++ 2021/05/index.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+
+function drawLine(point, lineType, grid) {
+  let start = 0;
+  let end = 0;
+
+  if (lineType === 'horizontal') {
+    start = point.y1 > point.y2 ? point.y2 : point.y1;
+    end = point.y1 > point.y2 ? point.y1 : point.y2;
+  } else {
+    start = point.x1 > point.x2 ? point.x2 : point.x1;
+    end = point.x1 > point.x2 ? point.x1 : point.x2;
+  }
+
+  for (start; start <= end; start++) {
+    let horizontalPointExists = grid.find((p) => (lineType === 'horizontal' && p.x === point.x1 && p.y === start));
+    let verticalPointExists = grid.find((p) => (lineType === 'vertical' && p.y === point.y1 && p.x === start));
+
+    if (horizontalPointExists) {
+      horizontalPointExists.point++;
+    } else if (verticalPointExists) {
+      verticalPointExists.point++;
+    } else {
+      grid.push({
+        x: lineType === 'horizontal' ? point.x1 : start,
+        y: lineType === 'horizontal' ? start : point.y1,
+        point: 1,
+      });
+    }
+  }
+
+  return grid;
+}
+
+async function part1 () {
+  const input = (await fs.promises.readFile('input.txt', 'utf-8'))
+    .split('\n')
+    .map((coord) => coord.split(' -> '))
+    .map((coord) => {
+      let start = coord[0].split(',');
+
+      let startX = +start[0];
+      let startY = +start[1];
+
+      let end = coord[1].split(',');
+
+      let endX = +end[0];
+      let endY = +end[1];
+
+      return {
+        x1: startX,
+        y1: startY,
+        x2: endX,
+        y2: endY,
+      };
+    })
+
+  let answer = 0;
+  let grid = [];
+
+  for (let coord of input) {
+    if (coord.x1 === coord.x2) {
+      // Horizontal line found - +1
+      grid = drawLine(coord, 'horizontal', grid);
+    }
+
+    if (coord.y1 === coord.y2) {
+      // Vertial line found - +1
+      grid = drawLine(coord, 'vertical', grid);
+    }
+  }
+
+  answer = grid.filter((v) => v.point >= 2).length;
+
+  console.log(`Part 1: ${answer}`);
+}
+
+// function part2() {
+//   const input = (await fs.promises.readFile('sample.txt', 'utf-8')).split('\n');
+//   let answer = 0;
+
+
+
+//   console.log(`Part 2: ${answer}`);
+// }
+
+part1();
+// part2();
